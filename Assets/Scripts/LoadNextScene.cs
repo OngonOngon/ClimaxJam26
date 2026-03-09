ba910c66e2149f3ee6a7f0b52bdbf20b0c974e2e
Jan Pospíšil <jenda.pospec@gmail.com>
2026-03-09 20:40:13 +0100

    Scenes order and bonus level

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Dubinci
+{
+    public class LoadNextScene : MonoBehaviour
+    {
+        public void NextScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
